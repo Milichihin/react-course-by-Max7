d398fc7d49e8a28ef228695c9fcccb2004f70ffa
Milichihin <32966900+Milichihin@users.noreply.github.com>
2021-06-08 07:50:06 +0300

    additional comment

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,6 +3,7 @@ import "./Card.css";
 function Card(props) {
   const classes = "card " + props.className;
   // some comment
+  // additional comment
   return <div className={classes}>{props.children}</div>;
 }
 export default Card;
